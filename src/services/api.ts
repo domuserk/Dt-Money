fc5fd7df72938caf83082d43e117af81a6f3cdb7
Ryan G. Lopes <rgdesenhos321@gmail.com>
2022-01-14 08:56:46 -0300

    Config env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL
+  baseURL:  process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_DEV : process.env.REACT_APP_API_URL
 })
